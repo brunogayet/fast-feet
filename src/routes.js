97198fe74d09d08d3a16afd48d329fe8fa3ee451
Bruno Gayet <brunogayet@gmail.com>
2020-02-27 00:35:49 -0400

    improvement: avatar users upload
    Import multer dependency
     Create controller and model class File
     Create migration File and
    Avatar's field in the Users table

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,17 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
 
 import RecipientController from './app/controllers/RecipientController';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 // [Create] Users Route
 routes.post('/users', UserController.store);
@@ -26,5 +30,7 @@ routes.post('/recipients', RecipientController.store);
 
 // [Update] Recipient Route
 routes.put('/recipients', RecipientController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
